c30d876404384c50b78fd538d0a92a5a9cce38b9
Iwan Rahman <1w3n.rahman@gmail.com>
2023-07-14 21:59:43 -0400

    Add operate function

--- script.js
+++ script.js
@@ -1,4 +1,19 @@
+//Global Variable
+let operandOne;
+let operandTwo;
+let operator;
+
+//Basic Operation Functions
 let add = (a,b) => a+b;
 let subtract = (a,b) => a-b;
 let multiply = (a,b) => a*b;
 let divide = (a,b) => a/b;
+
+function operate(a,b,op){
+  switch(op){
+    case '+': return add(a,b);
+    case '-': return subtract(a,b);
+    case '*': return multiply(a,b);
+    case '/': return divide(a,b);
+  }
+}
